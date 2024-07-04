ca0eb75208527f9e5cbd4401a3c18472ff7f3806
sofialopsil123 <80548975+sofialopsil123@users.noreply.github.com>
2024-07-04 17:29:05 +0200

    curriculum

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",(event)=>{
+
+    setTimeout(() =>{
+        (document.querySelector("#load-iframe-map").innerHTML)= `<iframe class="contact__iframe"  frameborder="0" scrolling="0" marginheight="0" marginwidth="0"  style="border:0;"  loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.0869590504735!2d-74.08297732538419!3d4.7549121952202835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f84525a3f7051%3A0xd414a6a2c7e965f8!2sCra.%2092%20%23157a65%2C%20Bogot%C3%A1!5e0!3m2!1ses-419!2sco!4v1699888472334!5m2!1ses-419!2sco" ></iframe> `;
+
+    } ,500);
+    
+
+});
